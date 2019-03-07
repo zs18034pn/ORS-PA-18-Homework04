89c942fef8acf82fae11fdc01f0378b548da364a
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-03-07 13:28:50 +0100

    task1

---
+++ task1.py
@@ -0,0 +1,15 @@
+"""
+===================   TASK 1   ====================
+* Name: Sum Number Digits
+*
+* Write a function `sum_digits` that will return
+* sum of digits for given integer number.
+* If passed value is invalid, function should
+* return -1 which indicates something went wrong.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Use main() function to test your solution.
+===================================================
+"""
