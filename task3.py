b441c2d53ca23d1026832e199cf1927e7e6ebb24
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-03-07 13:34:37 +0100

    task3

---
+++ task3.py
@@ -0,0 +1,17 @@
+"""
+===================   TASK 3   ====================
+* Name: Area Of Circle
+*
+* Write a function `area_of_circle` that will
+* return area enclosed by a circle of radius `r`.
+* Consider that only float value for radius will
+* be passed. Negative values should be considered
+* as typo and function should ignore sign of a
+* number.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Use main() function to test your solution.
+===================================================
+"""
